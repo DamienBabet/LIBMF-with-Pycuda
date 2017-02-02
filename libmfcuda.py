5ddc137ae8ce6e1f6c8cee704c1b1d9b2e3b545a
JulieDjidji <julie.djiriguian@ensae-paristech.fr>
2017-02-02 19:28:47 +0100

    Update libmfcuda.py

--- libmfcuda.py
+++ libmfcuda.py
@@ -26,11 +26,17 @@ p2=p.reshape((nbUsers+AjoutUsersFictifs,K))
 q2=q.reshape((K,nbFilms+AjoutFilmsFictifs))
 r=np.dot(p2,q2).T
 
-r_sparse = sp.rand(nbUsers+AjoutUsersFictifs,nbFilms+AjoutFilmsFictifs,0.1)
-r_data=np.array([r_sparse.row, r_sparse.col, r_sparse.data])
-r_data=np.transpose(r_data)
+tp=int((nbUsers+AjoutUsersFictifs)/nbBloc)
+tq=int((nbFilms+AjoutFilmsFictifs)/nbBloc)
 
-np.shape(r)
+r_sparse = sp.rand(nbUsers+AjoutUsersFictifs,nbFilms+AjoutFilmsFictifs,0.1)
+r_data=np.transpose(np.array([r_sparse.row, r_sparse.col, r_sparse.data]))
+df=pd.DataFrame(r_data, columns=("i","j","value"))
+df["numBlocLigne"] =df.apply(lambda row : int(row[0]/tp), axis = 1 )
+df["numBlocColonne"] =df.apply(lambda row : int(row[1]/tq), axis = 1 )
+v = pd.DataFrame(df.groupby(["numBlocLigne", "numBlocColonne"]).count())
+v = v.reset_index()[["numBlocLigne","numBlocColonne","value"]]
+v
 
 mod = SourceModule("""
     __global__ void block_update(float *p, float *q, float *r, float *v, int tP, int tQ, int K, int gamma, int lambdaP, int lambdaQ)
@@ -77,11 +83,13 @@ for l in range(nbIter):
     for i in range(1,nbBloc) :
         q_permut=np.append(q_permut,qEnBloc[L[i]].tolist())
     
-    # Mise en forme de R selon l'ordre de la permutation
-    r_permut
-    
-    # Creation de V vecteur des coordonnees des debuts de blocks sur R
-    
+    # Mise en forme de R selon l'ordre de la permutation et création de v_permut (vecteur des coordonnees des debuts de blocks sur R)
+    r_permut=np.array(df[(df["numBlocLigne"]==0) & (df["numBlocColonne"]==L[0])][[0,1,2]]).flatten()
+    v_permut=np.array(v[(v["numBlocLigne"]==0) & (v["numBlocColonne"]==L[0])][[0,1,2]]).flatten()
+    for i in range(1,nbBloc) :
+        r_permut=np.append(r_permut,np.array(df[(df["numBlocLigne"]==i) & (df["numBlocColonne"]==L[i])][[0,1,2]]).flatten()).flatten()
+        v_permut=np.append(v_permut,np.array(v[(v["numBlocLigne"]==i) & (v["numBlocColonne"]==L[i])][[0,1,2]]).flatten()).flatten()
+      
     # Transfert des données sur la GPU
     p_gpu = cuda.mem_alloc(p.nbytes)
     cuda.memcpy_htod(p_gpu, p)
